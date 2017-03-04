aa12c8e4dab49f3b9fb2e8aff316494aa09b622f
FridayDai <friday.dai@xplusz.com>
2017-03-04 17:31:15 +0800

    #start to do mission 2.

--- js/index.js
+++ js/index.js
@@ -2,46 +2,13 @@
  * Created by xplusz on 2/27/17.
  */
 
-function getTrueLength(string) {
-    var len = 0;
-    var str = string.replace(/\s/g,''); // 去除所有空格 trim()能去掉两边空格
 
-    for(var i = 0; i < str.length; i++) {
-        if(str.charCodeAt(i) > 256) {
-            len += 2; //中文
-        } else if(str.charCodeAt(i) < 256) {
-            len += 1; //英文
-        }
-    }
-    return len;
-}
+var showMessage = function(obj) {
+    var hintmessage = obj.nextElementSibling;
+    hintmessage.style.display = "table-row";
+};
 
-
-var submitBtn = document.getElementById("identify");
-var input = document.getElementById("username");
-
-
-submitBtn.onclick = function(e) {
-    var username = input.value;
-    var username_length = getTrueLength(username);
-
-    if(username_length == 0) {
-        document.getElementById("hintmessage").style.display = 'none';
-        document.getElementById("nullmessage").style.display = "table-row";
-        document.getElementById("rightmessage").style.display = "none";
-        input.className = 'wrong';
-        e.preventDefault();
-    } else if (username_length < 4 || username_length > 16) {
-        document.getElementById("hintmessage").style.display = 'table-row';
-        document.getElementById("nullmessage").style.display = "none";
-        document.getElementById("rightmessage").style.display = "none";
-        input.className = 'wrong';
-        e.preventDefault();
-    } else {
-        document.getElementById("hintmessage").style.display = 'none';
-        document.getElementById("nullmessage").style.display = "none";
-        document.getElementById("rightmessage").style.display = "table-row";
-        input.className = 'right';
-        e.preventDefault();
-    }
+var hideMessage = function(obj) {
+    var hintmessage = obj.nextElementSibling;
+    hintmessage.style.display = "none";
 };
